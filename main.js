71848149e164fe4e668ffeeeb891e46cb51b0796
Spencer Wallace <77997705+SpencerDWallace@users.noreply.github.com>
2021-10-12 07:54:33 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-var windowW = $(window).width(); var windowH = $(window).height(); var cnv;
+var windowW = $(window).width(); var windowH = $(window).height(); var cnv; var browserZoomLevel;
 var sidebarActive = false; var sidebar; var sidebarW = windowW / 40; var sidebarH = windowH / 25;
-var menuAnimation = false; var menuW; var menuH; var sideMenuExitBox; var header;
+var menuAnimation = false; var menuW; var menuH;  var header; var sideMenuExitBox; var sidebarBox;
+var s20; var s21; var wed; var numAlb = 3; var album = new Array(numAlb); var nullAlbum;
+let albumW; let albumH; let albumSpace; let photo; var previewsCreated = false;
 /*const targetElement = document.querySelector('#screen');
 bodyScrollLock.disableBodyScroll(targetElement);*/
 
 function setup()
 {
+    browserZoomLevel = (Math.round(window.devicePixelRatio * 100))/100;
     cnv = createCanvas(windowW, windowH);
     cnv.position(0,0);
+    s20 = "Summer '20"; s21 = "Summer '21"; wed = "Wedding";
+    //s20 = createElement('h1', "Summer '20"); s21 = createElement('h1', "Summer '21"); wed = createElement('h1', "Wedding");
+    menuW = windowW/4; menuH = windowH - 1; eleFont = windowH/40;
+    //s20.style('font-size', eleFont + 'px'); s21.style('font-size', eleFont + 'px'); wed.style('font-size', eleFont + 'px');
+/*    for(let i = 0; i < numAlb; i++){ */
+    let albumBoxes = new Array(numAlb);
+    let albumW = menuW/4; let albumH = albumW*2;
+    let albumSpace = albumW;
 
-    menuW = windowW/4; menuH = windowH - 1;
-
+    for(let i = 0; i < numAlb; i++)
+    {
+        albumBoxes[i] = new Box( (3*menuW)/8, i*albumH + albumW, (3*menuW)/8 + albumW, i*albumH + albumW + albumW + albumSpace/2 );
+    }
+    //photo = loadImage('./wedding_jacket_tryon.jpeg');
+    nullAlbum = new Album(null, null, null, null, null);
+    album[0] = new Album(s20, albumBoxes[0], 0, 0, null);
+    album[1] = new Album(s21, albumBoxes[1], 0, 0, null);
+    album[2] = new Album(wed, albumBoxes[2], 0, 0, null);
+    strokeCap(SQUARE);
 }
 
 let Box = class {
@@ -19,17 +38,16 @@ let Box = class {
     }
 };
 
-//tmbnl is the thumbnail image, descr is the album name
-let pAlbum = class {
-    constructor(tmbnl, descr, box) {
-        this.tmbnl; this.descr; this.box;
+let Album = class{
+    constructor(name, box, size, photos, title) {
+        this.name = name; this.box = box; this.size = size; this.photos = photos; this.title = title;
     }
-};
+}
 
 function draw()
 {
     clear();
-    background(222);
+    background(252,255,252);
     if(windowW > windowH)
         drawSidebarDesktop();
 
@@ -37,13 +55,23 @@ function draw()
 
 function mouseClicked()
 {
-    if(mouseX >= 0 && mouseX <= sidebarW && mouseY >= 0 && mouseY <= sidebarH && !sidebarActive)
+    if(sidebarBox != null && checkRectangle(sidebarBox) && !sidebarActive)
         sidebarActive = true;
-    if(sideMenuExitBox != null && mouseX >= sideMenuExitBox.x && mouseX <= sideMenuExitBox.x2 && mouseY >= sideMenuExitBox.y && mouseY <= sideMenuExitBox.y2) {
+    else if(sideMenuExitBox != null && checkRectangle(sideMenuExitBox)) {
         menuAnimation = false;
         sidebarActive = false;
+        previewsCreated = false;
+        removePreviews();
     }
 
+}
+
+function checkRectangle(box)
+{
+if(mouseX >= box.x && mouseX <= box.x2 && mouseY >= box.y && mouseY <= box.y2)
+    return true;
+
+return false;
 }
 
 function sideMenu()
@@ -64,8 +92,49 @@ function sideMenu()
         line(menuW - sidebarW * 0.75, sidebarH * 0.25, menuW - sidebarW * 0.25, sidebarH * 0.75);
         line(menuW - sidebarW * 0.25, sidebarH * 0.25, menuW - sidebarW * 0.75, sidebarH * 0.75);
         strokeWeight(1);
+        drawAlbums();
+
+}
+
+function removePreviews()
+{
+    for (let i = 0; i < numAlb; i++)
+    {
+        album[i].photos.remove();
+        album[i].title.remove();
+    }
+}
+
+function drawAlbums() {
+
+    for (let i = 0; i < numAlb; i++) {
+        x = album[i].box.x;
+        x2 = album[i].box.x2 - album[i].box.x;
+        y = album[i].box.y;
+        y2 = album[i].box.y2 - album[i].box.y - x2 / 2;
+        stroke(150);
+        fill(20);
+        rect(x - 5, y - 5, x2 + 10, x2 + 10);
+        fill('rgba(50%,30%,10%,0.3)');
+        rect(x - 15, y - 15, x2 + 30, x2 + 30);
+        if (!previewsCreated) {
+            album[i].photos = createImg('https://spencerdwallace.github.io/Home/wedding_jacket_tryon.jpeg', 'wedding jacket tryon', 'anonymous');
+            album[i].photos.size(x2, x2)
+            album[i].photos.position(x, y)
 
 
+            album[i].title = createElement('h1', '' + album[i].name);
+            album[i].title.style('font-size', eleFont + 'px');
+            //album[i].title.style('width', x2 + 'px');
+            //album[i].title.parent(album[i].photos);
+            //album[i].title.center();
+            album[i].title.position(x, album[i].box.y2 - x2 / 2);
+            //album[i].title.center();
+            //albumBoxes[i] = new Box( (3*menuW)/8, i*albumH + albumSpace, albumW, albumW + albumSpace/2  );
+        }
+
+    }
+    previewsCreated = true;
 }
 
 function sleep(ms) {
@@ -107,6 +176,7 @@ async function drawSidebarDesktop()
         fill('#77AAFF');
         stroke(175);
         sidebar = rect(0, 0, sidebarW, sidebarH);
+        sidebarBox = new Box(0,0, sidebarW, sidebarH);
         fill(0);
         stroke(1);
         rect(sidebarW / 4, (sidebarH / 3) - 1, sidebarW / 2, 2);
